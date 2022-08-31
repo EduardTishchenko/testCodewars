129232265f9e9ab97ca041585950f88298345339
Eduard <eduartishchenkoseoppc@gmail.com>
2022-08-31 19:57:20 +0300

    first commit

---
+++ test01.js
@@ -0,0 +1,7 @@
+function reverseWords(str){
+    str.Ar = ["hello world!","`yoda doesn't speak like this`","foobar","kata editor","row row row your boat"]
+  
+   
+   return str.split(" ").reverse().join(" ");
+    // reverse those words
+ }
